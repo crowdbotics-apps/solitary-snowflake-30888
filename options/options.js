9f335d5d2ab1f80b9007a2768f2bc0e7cc03f941
Crowdbotics <team@crowdbotics.com>
2021-09-27 02:39:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "solitary_snowflake_30888",
+  url: "https://solitary_snowflake_30888.botics.co",
+  api: "https://solitary_snowflake_30888.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
